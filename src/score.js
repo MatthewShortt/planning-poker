d9217276b7f64b756dcdca68c15f278cecc9bae7
Matthew Shortt <Matthew.Shortt@intelliware.ca>
2019-05-21 13:28:16 -0400

    Add verification to url parameter

--- src/score.js
+++ src/score.js
@@ -5,9 +5,17 @@ window.onload = function() {
 
 function getScoreValue() {
     var href = window.location.href;
-    var url = new URL (href);
+    var url = new URL(href);
     let value = url.searchParams.get('value');
-    return value !== null ? value : 'Value not specified!';
+    return validateScore(value);
+}
+
+function validateScore(score) {
+    if(score === null || score === undefined || score === '') {
+        return 'Value not specified!';
+    } else {
+        return !isNaN(score) ? score : 'Value is not a number!';
+    }
 }
 
 function setScoreValue(scoreValue) {
